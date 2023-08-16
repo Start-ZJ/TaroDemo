cd69d5d504a921b0b85cf3ccf048e58a43ef8590
Start-ZJ <847003171@qq.com>
2023-08-16 23:06:14 +0800

    用户页面

--- src/pages/personalParticulars/personalParticulars.jsx
+++ src/pages/personalParticulars/personalParticulars.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react'
-import { View, Picker } from '@tarojs/components'
-import {  } from 'taro-ui'
+import { View, Image } from '@tarojs/components'
+import { AtList, AtListItem } from 'taro-ui'
 import './index.scss';
 const ListTitleLine = (props) => {
   const { label } = props;
@@ -14,9 +14,48 @@ export default class PersonalParticulars extends Component {
   }
   componentWillMount() {
   }
+  renderUserInfoDom = () => {
+    return (<View className='renderUserInfoDom'>
+      <Image className='renderUserInfoDomUserInfoPng' src={require('./images/userInfo.png')} />
+      <View className='renderUserInfoDom-User'>
+        <View className='renderUserInfoDom-User-Text'>
+          <View className='renderUserInfoDom-User-Text-Name'></View>
+          <View className='renderUserInfoDom-User-Text-Mobile'></View>
+        </View>
+        <View className='renderUserInfoDom-User-Png'>
+          <Image className='renderUserInfoDomUserInfoPng-User' src={'ss'} />
+        </View>
+      </View>
+      <View className='renderUserInfoDom-BillInfo'>
+        <View className='renderUserInfoDom-BillInfo-Item'>
+          <View className='renderUserInfoDom-BillInfo-Label'>未开始</View>
+          <View className='renderUserInfoDom-BillInfo-Num'>10</View>
+        </View>
+        <View className='renderUserInfoDom-BillInfo-Item'>
+          <View className='renderUserInfoDom-BillInfo-Label'>进行中</View>
+          <View className='renderUserInfoDom-BillInfo-Num'>10</View>
+        </View>
+        <View className='renderUserInfoDom-BillInfo-Item'>
+          <View className='renderUserInfoDom-BillInfo-Label'>已结束</View>
+          <View className='renderUserInfoDom-BillInfo-Num'>10</View>
+        </View>
+      </View>
+    </View>)
+  }
+  renderEventLineDom = () => {
+    return (<View>
+      <AtList>
+        <AtListItem title='未开始' arrow='right' onClick={() => { }} />
+        <AtListItem title='进行中' arrow='right' onClick={() => { }} />
+        <AtListItem title='已结束' arrow='right' onClick={() => { }} />
+      </AtList>
+    </View>)
+  }
   render() {
     return (
-      <View className='personalParticulars'>personalParticulars
+      <View className='personalParticulars'>
+        {this.renderUserInfoDom()}
+        {this.renderEventLineDom()}
       </View>
     )
   }
