fa4fe03a2db12986ea34999aedd50d71cd1a9866
Start-ZJ <847003171@qq.com>
2023-08-20 22:35:14 +0800

    加了微信登录获取用户信息的调试

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import { View, Text, Image } from '@tarojs/components'
+import { View, Text, Image, Button } from '@tarojs/components'
 import { AtIcon } from 'taro-ui'
 import Taro from '@tarojs/taro'
 import './index.scss'
@@ -39,17 +39,33 @@ export default class Index extends Component {
     }
   }
   componentWillMount() {
-
+    wx.login({
+      success(res) {
+        if (res.code) {
+          console.log('Index  success  res.code---->', res.code)
+          // 拿到code 请求 自定义 下文 2中 服务端（开发者服务器） 获取 openid、session_key、unionid
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+    wx.getUserInfo({
+      success: function (res) {
+        console.log('Index  componentWillMount  res---->', res)
+        var userInfo = res.userInfo
+        var nickName = userInfo.nickName
+        var avatarUrl = userInfo.avatarUrl
+        var gender = userInfo.gender //性别 0：未知、1：男、2：女
+        var province = userInfo.province
+        var city = userInfo.city
+        var country = userInfo.country
+      }
+    })
   }
-
   componentDidMount() { }
-
   componentWillUnmount() { }
-
   componentDidShow() { }
-
   componentDidHide() { }
-
   renderFooterDOM = (item) => {
     const { value, content, Icon, fun } = item;
     const Icon2 = Icon;
@@ -59,20 +75,44 @@ export default class Index extends Component {
         <View className='homeFooterContent'>{content}</View>
       </View>)
   }
+  tobegin = (res) => {
+    console.log('Index  res---->', res)
+    // 保存用户信息微信登录
+    // Taro.setStorage({
+    //   key: "userinfo",
+    //   data: res.detail.userInfo
+    // });
+  }
   render() {
     const { type } = this.state;
     const footerAry = type === 'user' ? userFooterAry : adminFooterAry;
     return (
       <View className='index'>
         <View className='homeBox'>
-          <View className='homeBody'>
+          {/* <View className='homeBody'>
             <Image className='homeBodyPng' src={require('./images/background.png.jpg')} />
-          </View>
+          </View> */}
+          {<Button
+            className='btn'
+            openType='getUserInfo'
+            // onGetUserInfo={this.tobegin}
+            type='primary'
+            lang='zh_CN'
+          >
+            开启缘分
+          </Button>}
+          {/* <Button openType="getPhoneNumber" bindgetphonenumber="getPhoneNumber">s</Button> */}
+          <Button
+            className='login-buttoNor'
+            type="primary"
+            open-type="getUserInfo"
+            onGetUserInfo={this.tobegin}
+          >微信获取用户信息</Button>
           <View className='homeFooter'>
             {footerAry.map(item => this.renderFooterDOM(item))}
           </View>
         </View>
-      </View>
+      </View >
     )
   }
 }
